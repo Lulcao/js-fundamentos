766090fa50b6cbe58992698f424424a8ec7b23ed
Lucas Cotrim MoreiraBhering <16385955770@cefet-rj.br>
2022-11-30 15:54:25 -0300

    codigo var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,8 @@
+const numero = 456;
+const numString = "456";
+//const numString = Number("456");
+
+//Number(); --> transforma str em num
+//String(); --> transforma num em str
+console.log(numero + Number(numString));
+
---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+
+const usuariologado = true;
+const contapaga = false;
+
+//truthy or falsy
+
+// 0 => false
+// 1 => true
+
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "teste";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
